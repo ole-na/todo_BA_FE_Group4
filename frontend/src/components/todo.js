e9500efb6a66a9331189207efb228ac6442b5c3f
ole-na <82842056+ole-na@users.noreply.github.com>
2021-05-18 10:13:43 +0200

    Remove commented out code

--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -3,57 +3,10 @@ import React, { useEffect, useRef, useState } from "react";
 function Todo({ todo, onDescription, onDelete, onAdvance }) {
   const [showDelete, addShowDelete] = useState(false);
 
-  /* const btnDeleteRef = useRef(null);
-  function distance(x1, y1, x2, y2) {
-    const dx = x1 - x2;
-    const dy = y1 - y2;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
-
-  useEffect(() => {
-    const btn = btnDeleteRef.current.querySelector(".btn");
-
-    const btnFront = btn.querySelector(".btn-front"),
-      btnYes = btn.querySelector(".btn-back .yes"),
-      btnNo = btn.querySelector(".btn-back .no");
-
-    btnFront.addEventListener("click", function (event) {
-      const mx = event.clientX - btn.offsetLeft,
-        my = event.clientY - btn.offsetTop;
-
-      const w = btn.offsetWidth,
-        h = btn.offsetHeight;
-
-      const directions = [
-        { id: "top", x: w / 2, y: 0 },
-        { id: "right", x: w, y: h / 2 },
-        { id: "bottom", x: w / 2, y: h },
-        { id: "left", x: 0, y: h / 2 },
-      ];
-
-      directions.sort(function (a, b) {
-        return distance(mx, my, a.x, a.y) - distance(mx, my, b.x, b.y);
-      });
-
-      btn.setAttribute("data-direction", directions.shift().id);
-      btn.classList.add("is-open");
-    });
-
-    btnYes.addEventListener("click", function (event) {
-      btn.classList.remove("is-open");
-    });
-
-    btnNo.addEventListener("click", function (event) {
-      btn.classList.remove("is-open");
-    });
-  }, []);
-  */
-
   return (
     <div key={todo.id} className="todo-card">
       <h3 className="description">{todo.description}</h3>
       <div className="buttons">
-        {/*<div className="buttons" ref={btnDeleteRef}>*/}
         <button className="button glow-on-hover" onClick={onDescription}>
           Description
         </button>
@@ -65,7 +18,6 @@ function Todo({ todo, onDescription, onDelete, onAdvance }) {
           Advance
         </button>
 
-        {/*<button onClick={() => onDelete(todo)}>Delete</button>*/}
         <div className="btn">
           {showDelete && (
             <div className="btn-back">
